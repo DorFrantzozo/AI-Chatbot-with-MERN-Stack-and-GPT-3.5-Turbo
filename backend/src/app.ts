20864b8cd32d8b279c4aa5009f3810e3f63a408b
DorFrantzozo <dorfrant@gmail.com>
2024-07-16 14:06:42 +0300

    fix3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,10 +12,5 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//remove it in production!!!!!!
-app.use(morgan("dev"));
-
 app.use("/api/v1/", appRouter);
 export default app;
-
-//stopped 3:19
