3e59c2ae795166988031c3739ecc56e0111c42e8
dor <dorfrant@gmail.com>
2024-07-22 11:50:51 +0300

    cors fix16

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,8 +11,8 @@ const app = express();
 //   "https://ai-chatbot-client-three.vercel.app",
 //   "https://ai-chatbot-client-l9cgsmubm-dors-projects-2f5d6a31.vercel.app",
 // ];
-// app.use(cors());
-// app.options("*", cors());
+app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
