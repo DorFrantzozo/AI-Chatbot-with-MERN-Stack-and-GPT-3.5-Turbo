00c2627649bc2c982333b7ca1f84ac30245b259b
dor <dorfrant@gmail.com>
2024-07-22 11:34:23 +0300

    cors fix10

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -9,14 +9,7 @@ const allowedOrigins = [
     "https://ai-chatbot-client-three.vercel.app",
     "https://ai-chatbot-client-l9cgsmubm-dors-projects-2f5d6a31.vercel.app",
 ];
-app.use(cors({
-    origin: [
-        "https://ai-chatbot-client-three.vercel.app",
-        "https://ai-chatbot-client-l9cgsmubm-dors-projects-2f5d6a31.vercel.app",
-        ,
-    ],
-    credentials: true,
-}));
+app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
